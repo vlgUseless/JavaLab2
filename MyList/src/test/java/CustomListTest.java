7def1258e8843113bebe17c77df4aa90e5fabe9a
Vladislav Gaar <gaarvlad@gmail.com>
2024-11-05 16:09:15 +0300

    Added testAddAndGet

--- MyList/src/test/java/CustomListTest.java
+++ MyList/src/test/java/CustomListTest.java
@@ -16,4 +16,22 @@ public class CustomListTest {
         assertEquals(customList.size(), Integer.valueOf(0));
         assertEquals(customList.size(), arrayList.size());
     }
+
+    @Test
+    void testAddAndGet() {
+        CustomList<Integer> customList = new CustomList<>();
+        List<Integer> arrayList = new ArrayList<>();
+
+        customList.add(2);
+        customList.add(5);
+
+        arrayList.add(2);
+        arrayList.add(5);
+
+        assertEquals(customList.size(), Integer.valueOf(2));
+        assertEquals(customList.size(), arrayList.size());
+
+        assertEquals(customList.get(0), arrayList.get(0));
+        assertEquals(customList.get(1), arrayList.get(1));
+    }
 }
