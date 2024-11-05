e4b7e9d4d3a17a1a543bdf58f446f28bad50e9ff
Vladislav Gaar <gaarvlad@gmail.com>
2024-11-05 16:15:24 +0300

    Added testRemoveIndex

--- MyList/src/test/java/CustomListTest.java
+++ MyList/src/test/java/CustomListTest.java
@@ -76,4 +76,24 @@ public class CustomListTest {
         assertEquals(customList.get(0), arrayList.get(0));
         assertEquals(customList.get(1), arrayList.get(1));
     }
+
+    @Test
+    void testRemoveIndex() {
+        CustomList<Integer> customList = new CustomList<>();
+        List<Integer> arrayList = new ArrayList<>();
+
+        customList.add(2);
+        customList.add(5);
+        customList.remove(1);
+
+        arrayList.add(2);
+        arrayList.add(5);
+        arrayList.remove(1);
+
+        assertEquals(customList.size(), Integer.valueOf(1));
+        assertEquals(customList.size(), arrayList.size());
+
+        assertEquals(customList.get(0), Integer.valueOf(2));
+        assertEquals(customList.get(0), arrayList.get(0));
+    }
 }
