a894787ba6a14730ec02cbaeed9f344a1517e507
Vladislav Gaar <gaarvlad@gmail.com>
2024-11-05 16:00:31 +0300

    Added testEmpty

--- MyList/src/test/java/CustomListTest.java
+++ MyList/src/test/java/CustomListTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CustomListTest {
+
+    @Test
+    void testEmpty() {
+        CustomList<Integer> customList = new CustomList<>();
+        List<Integer> arrayList = new ArrayList<>();
+
+        assertEquals(customList.isEmpty(), arrayList.isEmpty());
+        assertEquals(customList.size(), Integer.valueOf(0));
+        assertEquals(customList.size(), arrayList.size());
+    }
 }
